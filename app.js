9e80f4281e80f85b1d600e9f753ce13b23b6324b
Lin To <linxuanto@gmail.com>
2019-08-07 13:22:49 +0800

    changes to login

--- app.js
+++ app.js
@@ -1,53 +1,62 @@
 //app.js
 App({
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-    // <----------login function----------->
-    this.tologin()
+    console.log('launching')
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
+    this.checkLogin(); 
+    
   },
 
-  toLogin: function () {
-  const host = this.globalData.url;
+  getUserInfo: function (res) {
+
+    let that = this
+    const host = this.globalData.url;
     wx.request({
       url: host + 'login',
-      method: 'post',
+      method: 'POST',
       data: {
         code: res.code
       },
       success: (res) => {
-        console.log(res)
-        this.globalData.userId = res.data.userId
+        that.globalData.user = res.data;
+        
       },
+    })
+    // create_table "users", force: : cascade do | t |
+    //   t.string "wx_id"
+    // t.string "wx_name"
+    // t.string "wx_avatar"
+    // t.string "phone_number"
+    // t.boolean "seller_complete"
+    // t.datetime "created_at", null: false
+    // t.datetime "updated_at", null: false
+  },
+
+  checkLogin: function() {
+    let that = this
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          wx.getUserInfo({
+            success: res => {
+              this.globalData.userInfo = res.userInfo
+              
+              wx.switchTab({
+                url: '../home/home'
+              })
+
+            }
+          })
+        } else {
+
+          wx.login({
+            success: res => {
+              that.getUserInfo(res)
+            }
+          })
+
+        }
+      }
     })
   },
   
