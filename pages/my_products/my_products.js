b67ad62c84b198333e8ab605dcea9f8118d1e3e4
Lin To <linxuanto@gmail.com>
2019-08-09 15:08:07 +0800

    work in progress, dynamic cards but edit, delete still required

--- pages/my_products/my_products.js
+++ pages/my_products/my_products.js
@@ -13,7 +13,28 @@ Page({
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
+    let page = this;
+    const userId = app.globalData.userId;
+    console.log('userID',userId),
+    wx.request({
+      // url: `http://localhost:3000/api/v1/users/${userId}`,
+      url: `${app.globalData.url}users/${userId}`,
+      success: function (res) {
+        console.log('res', res);
+        var info_arr = res.data.products;
+        var obtained = [];
+        for (var i = 0; i < info_arr.length; i++) {
+          var product_name = info_arr[i].name;
+          var product_img = info_arr[i].img_url;
+          var product_id = info_arr[i].id;
+          var product_description = info_arr[i].description;
 
+          page.setData({
+            product_name, product_img, product_id, product_description
+          });
+        }
+      }
+    });
   },
 
   /**
